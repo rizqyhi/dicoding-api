e7a843e63b7cfd0444de443dc89beaa828c4b8cd
Rizqy  H <rizqy@dicoding.com>
2020-09-01 22:51:47 +0700

    Initial data fetching

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const fs = require('fs')
 const coursesFetcher = require('./src/fetchers/coursesFetcher');
 
 (async () => {
     const courses = await coursesFetcher.run()
 
-    console.log(courses)
+    fs.writeFileSync('./data/courses.json', JSON.stringify(courses, null, 2))
 })()
