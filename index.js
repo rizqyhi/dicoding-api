71dd89b12a17a760f4e12b807c3a5f51e64ee0fe
Rizqy Hidayat <rizqy22@gmail.com>
2020-09-12 11:48:44 +0700

    Run learning paths fetcher

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const fs = require('fs')
 const coursesFetcher = require('./src/fetchers/coursesFetcher');
+const learningPathsFetcher = require('./src/fetchers/learningPathsFetcher');
+
+const fetchCourses = async () => {
+    console.log('⏳ Fetching courses')
+    const courses = await coursesFetcher.run()
+    fs.writeFileSync('./data/courses.json', JSON.stringify(courses, null, 2))
+    console.log('🗄 Courses fetched and saved')
+}
+
+const fetchLearningPaths = async () => {
+    console.log('⏳ Fetching learning paths')
+    const learningPaths = await learningPathsFetcher.run()
+    fs.writeFileSync('./data/learning-paths.json', JSON.stringify(learningPaths, null, 2))
+    console.log('🗄 Learning paths fetched and saved')
+}
 
 (async () => {
-    const courses = await coursesFetcher.run()
-
-    fs.writeFileSync('./data/courses.json', JSON.stringify(courses, null, 2))
+    await fetchCourses()
+    await fetchLearningPaths()
 })()
